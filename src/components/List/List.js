c6b23462ac821d2892e092a019703e753d9f0953
Joe Fogg <GJF610@gmail.com>
2018-01-23 20:37:51 -0500

    houses

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -24,11 +24,177 @@ Granted 2008</p>
 
 <p className="citylist col-md-4">
 <h5>CALIFORNIA</h5>
-Eureka Sisters of Perpetual Indulgence,
+Eureka Sisters of Perpetual Indulgence
 Abbey of the Big Red Wood
 Granted 2007</p>
 
-</div>
+<p className="citylist col-md-4">
+<h5>CALIFORNIA</h5>
+L.A. Sisters,
+Los Angeles, California
+Granted 1996
+</p>
+
+<p className="citylist col-md-4">
+<h5>CALIFORNIA</h5>
+The Russian River Sisters of Perpetual Indulgence
+Russian River, California
+Granted 2007</p>
+
+<p className="citylist col-md-4">
+<h5>CALIFORNIA</h5>
+The Capital City Sisters
+Sacramento, California
+Granted 2016</p>
+
+<p className="citylist col-md-4">
+<h5>CALIFORNIA</h5>
+San Diego Sisters of Perpetual Indulgence
+The Asylum of the Tortured Heart
+San Diego, California
+Granted 2006
+</p>
+
+<p className="citylist col-md-4">
+<h5>DISTRICT OF COLUMBIA</h5>
+The DC Sisters
+Washington, DC
+Granted 2016</p>
+
+<p className="citylist col-md-4">
+<h5>FLORIDA</h5>
+First Coast Sisters
+Jacksonville FL
+Granted 2016</p>
+
+
+<p className="citylist col-md-4">
+<h5>FLORIDA</h5>
+The Orlando Sisters
+Abbey of St. Gertrude
+Orlando FL
+Granted 2011
+</p>
+
+<p className="citylist col-md-4">
+<h5>FLORIDA</h5>
+Tampa Bay Sisters of Perpetual Indulgence
+Tampa Bay, Florida
+Granted 2005
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>FLORIDA</h5>
+South Florida Sisters
+Ft Lauderdale, FL
+Granted 2012
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>GEORGIA</h5>
+Atlanta Sisters of Perpetual Indulgence
+Atlanta, GA 
+Granted 2011
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>ILLINOIS</h5>
+The Abbey of the Windy City Sisters
+Chicago IL 
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>KENTUCKY</h5>
+Derby City Sisters
+Louisville KY
+Granted 2016
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>LOUSIANA</h5>
+Big Easy Sisters
+New Orleans LA
+Granted 2015
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>MASSACHUSETTS </h5>
+Boston Sisters of Perpetual Indulgence
+Convent of the Commonwealth
+Granted 2009
+Boston, Massachusetts
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>MICHIGAN </h5>
+Motor City Sisters
+Detroit MI
+Granted 2016
+
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>NEVADA </h5>
+Las Vegas Sisters of Perpetual Indulgence,
+ Holy Order Sin Sity Sisters
+Las Vegas, Nevada
+Granted 2006
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>RENO </h5>
+Reno’s Biggest Little Sisters, Inc.
+Reno, NV
+Granted 2010
+
+</p>
+
+
+<p className="citylist col-md-4">
+<h5>NEW YORK</h5>
+New York City Sisters
+New York, New York
+
+</p>
+
+
+<p className="citylist col-md-4">
+<h5>MISSOURI</h5>
+The Gateway Sisters of Indulgence
+St. Louis, Missouri
+Granted 2010
+
+</p>
+
+
+<p className="citylist col-md-4">
+<h5>NORTH CAROLINA</h5>
+Greensboro NC
+Granted 2015
+
+</p>
+
+
+<p className="citylist col-md-4">
+<h5>OHIO</h5>
+Rock and Roll City Sisters
+Cleveland OH
+Granted 2014
+</p>
+
+
+
+
+// </div>
 </div>
 
  	);
