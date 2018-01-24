7fe5535046726fe4372f0fbe0bc24dda6c6a57e0
Joe Fogg <GJF610@gmail.com>
2018-01-23 21:07:22 -0500

    DONE

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -189,12 +189,256 @@ Granted 2015
 Rock and Roll City Sisters
 Cleveland OH
 Granted 2014
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>OKLAHOMA</h5>
+The OKLAHOMA City Sisters
+Oklahoma City OK
+2012
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>OKLAHOMA</h5>
+Tulsa OK
+Granted 2013
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>OREGON</h5>
+Portland Sisters of Perpetual Indulgence,
+The Order of Benevolent Bliss
+Portland, Oregon
+Founded: 2006
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>TENNESSEE</h5>
+Music City Sisters
+Nashville, Tennessee
+Granted 2010
+
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>TEXAS</h5>
+Weird City Sisters
+Austin TX
+Granted 2014
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>TEXAS</h5>
+Abbey of the Lone Star
+Dallas TX
+Granted 2011
+
+</p>
+
+<p className="citylist col-md-4">
+<h5> UTAH </h5>
+Zion Curtain Sisters
+Salt Lake City UT
+Granted 2014
+
+</p>
+
+<p className="citylist col-md-4">
+<h5> Washington </h5>
+The Sisters of Perpetual Indulgence,
+The Abbey Of Saint Joan
+Seattle, Washington
+Founded: 1996
+
+</p>
+
+<p className="citylist col-md-4">
+<h5> WISCONSIN </h5>
+Mad City Sisters
+Madison WI
+Granted 2015
+
+</p>
+
+<p className="citylist col-md-4">
+<h5> WISCONSIN</h5>
+Brew City Sisters
+Milwaukee WI
+Granted 2011
+
+</p>
+
+<p className="citylist col-md-4">
+<h5> UNITED STATES MISSIONS</h5>
+Missionary Order of Perpetual Indulgence
+Tennessee, Iowa, Chicago, Philadelphia
+Founded: 1996
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>AUSTRALIA </h5>
+The Order of Perpetual Indulgence
+Sydney, Australia
+Founded: 1982
+
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>AUSTRALIA </h5>
+The Order of Perpetual Indulgence in Australia, Perth
+Perth, Australia
+Founded: 1990
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>AUSTRALIA</h5>
+The Order of Perpetual Indulgence in Australia, Inc
+Adelaide, Australia
+Founded: 1992
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>CANADA </h5>
+Vancouver BC CN
+Granted 2012
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>COLOMBIA</h5>
+Hermanitas de la Perpetua Indulgencia
+Seccional, Colombia
+
 </p>
 
 
+<p className="citylist col-md-4">
+<h5>FRANCE </h5>
+Les Soeurs de la Perpétuelle Indulgence, Couvent de Paris
+Paris, France
+Founded: 1989
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>FRANCE </h5>
+Les Soeurs de Perpétuelle Indulgence, Couvent de Paname
+Paris, France
+Founded: 1996
 
 
-// </div>
+</p>
+
+<p className="citylist col-md-4">
+<h5>FRANCE</h5>
+Les Soeurs de Perpétuelle Indulgence, Couvent des Chenaies
+
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>GERMANY </h5>
+Orden der Schwestern der Perpetuellen Indulgenz e.V.,
+Erzmutterhaus Sankta Melitta Iuvenis zu Berlin
+Berlin, Germany
+Fonded: 1991 (1993, 2003; Reunion of S.P.I. and O.P.I. Berlin in 2007)
+
+
+</p>
+
+<p className="citylist col-md-4">
+<h5> GERMANY</h5>
+Die Schwestern der Perpetuellen Indulgenz zu Hamburg
+Haus Notre Dame Sainte Diana
+Hamburg, Germany
+Founded: 1996
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>GERMANY</h5>
+Die Schwestern der Perpetuellen Indulgenz e.V.
+Haus Sancta Maria Penetrantia
+Köln, Germany
+Founded: 1997
+S.P.I. e.V. Köln
+
+</p>
+
+<p 
+className="citylist col-md-4">
+<h5> SWITZERLAND </h5>
+Der Orden der Perpetuellen Indulgenz
+Zürich, Switzerland
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>UNITED KINGDON </h5>
+The House of Common Sluts
+London, United Kingdom
+Facebook page
+
+</p>
+
+<p className="citylist col-md-4">
+<h5> UNITED KINGDOM </h5>
+The Sisters of Perpetual Indulgence, Manchester
+Manchester, United Kingdom
+
+</p>
+
+<p className="citylist col-md-4">
+<h5> UNITED KINGDOM </h5>
+The Sisters of Perpetual Indulgence, Sussex
+Sussex, United Kingdom
+The Abbey of the Dreaming Spires
+Oxford, United Kingdom
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>UNITED KINGDOM  </h5>
+The Sanctuary of the Sisters of Perepetual Contumaciousness
+Rockall, United Kingdom
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>SCOTLAND</h5>
+The Order of Perpetual Indulgence, Convent of Dunn Eideann
+Edinburgh, Scotland
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>SCOTLAND</h5>
+The Order of Perpetual Indulgence, Convent of Moravia
+Elgin, Scotland
+
+
+</p>
+
+<p className="citylist col-md-4">
+<h5>Uruguay</h5>
+Uruguay Convento Mater Admirabilis
+Orden de San Felipe y Santiago de Montevideo
+Parroquia de la Inmaculada de Jádraque
+San Felipe y Santiago de Montevideo, Uruguay
+
+</p>
+
+</div>
 </div>
 
  	);
